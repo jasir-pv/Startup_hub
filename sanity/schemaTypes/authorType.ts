3419630111f33118a54bd4e86ca7185e83e7f4d6
jasir-pv <jasirahsanpv40@gmail.com>
2024-11-25 19:45:34 +0530

    Feat: Sanity Schema Creation

---
+++ sanity/schemaTypes/authorType.ts
@@ -0,0 +1,44 @@
+import {UserIcon} from '@sanity/icons'
+import {defineArrayMember, defineField, defineType} from 'sanity'
+
+export const authorType = defineType({
+  name: 'author',
+  title: 'Author',
+  type: 'document',
+  icon: UserIcon,
+  fields: [
+    defineField({
+      name: 'id',
+      type: 'number',
+    }),
+    defineField({
+      name: 'name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'username',
+      type: 'string',
+     
+    }),
+    defineField({
+      name: 'email',
+      type: 'image',
+      
+    }),
+    defineField({
+      name: 'image',
+      type: 'url',
+    
+    }),
+    defineField({
+      name: 'bio',
+      type: 'text',
+    }),
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+    },
+  },
+})
