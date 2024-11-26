39515fa7ced104c5017a3bf50eb73a80c732e39f
jasir-pv <jasirahsanpv40@gmail.com>
2024-11-26 22:40:09 +0530

    Search queries fixed

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,7 +1,9 @@
 
 import { defineQuery } from "next-sanity";
 
-export const STARTUPS_QUERY = defineQuery(`*[_type == "startup" && defined(slug.current)] | order(_createdAt desc) {
+export const STARTUPS_QUERY = defineQuery(`*[_type == "startup" && defined(slug.current) 
+  && !defined($search)|| title match $search || 
+  category match $search || author->name match $search ] | order(_createdAt desc) {
     _id,
     title,
     slug,
@@ -16,5 +18,5 @@ export const STARTUPS_QUERY = defineQuery(`*[_type == "startup" && defined(slug.
     description,
     category,
     image
-  }`);
+  }`);              
   
