5dd67f63d736c68f35365b4f3a54fe92b6592ccf
jasir-pv <jasirahsanpv40@gmail.com>
2024-11-27 20:00:45 +0530

    Startup page completed

--- components/View.tsx
+++ components/View.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import Ping from './Ping'
+import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/queries'
+import { client } from '@/sanity/lib/client'
 
 
 
-const View = ({id}: {id: string}) => {
+
+const View = async ({id}: {id: string}) => {
+
+  const { views: totalViews } = await client
+    .withConfig({ useCdn: false })
+    .fetch(STARTUP_VIEWS_QUERY, { id });
+
+    // TODO: Update the number of views
+
   return (
     <div className='view-container'>
-      <div className="absolute -top-2 -right-2"></div>
-      view
+      <div className="absolute -top-2 -right-2">
+
+      <Ping/>
+      </div>
+      <p className="view-text">
+        <span className='font-black'> views: {totalViews}</span>
+      </p>
     </div>
   )
 }
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -39,3 +39,7 @@ export const STARTUPS_QUERY = defineQuery(`*[_type == "startup" && defined(slug.
     image,
     pitch
   }`)
+
+  export const STARTUP_VIEWS_QUERY= defineQuery( `*[_type == 'startup' && _id==$id][0]{
+    _id,views
+    } `)
