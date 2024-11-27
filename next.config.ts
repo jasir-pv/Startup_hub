0184aa484bb8e523ddec8bdd5aa367e139e79ac0
jasir-pv <jasirahsanpv40@gmail.com>
2024-11-27 11:04:08 +0530

    understadin partials pre-rendering

--- next.config.ts
+++ next.config.ts
@@ -5,13 +5,22 @@ const nextConfig: NextConfig = {
 
   images:{
     dangerouslyAllowSVG:true,
+
     remotePatterns:[
       {
         protocol: 'https',
         hostname:'*'
       }
     ]
-  }
+  },
+  experimental: {
+   
+  },
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: "bottom-right",
+  },
 };
 
 export default nextConfig;
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -20,3 +20,22 @@ export const STARTUPS_QUERY = defineQuery(`*[_type == "startup" && defined(slug.
     image
   }`);              
   
+
+  export const STARTUP_BY_ID_QUERY =
+  defineQuery(`*[_type == "startup" && _id==$id][0]   {
+    _id,
+    title,
+    slug,
+    _createdAt,
+    author {
+      _id,
+      name,
+      image,
+      bio
+    },
+    views,
+    description,
+    category,
+    image,
+    pitch
+  }`)
