638798faf25a9efba1edad189e915315a88161f6
jasir-pv <jasirahsanpv40@gmail.com>
2024-12-02 15:54:41 +0530

    Feat: Create startup form server creation

--- lib/utils.ts
+++ lib/utils.ts
@@ -14,3 +14,7 @@ export function formatDate(date: string){
     year: 'numeric',
   })
 }
+
+export function parseServerActionResponse<T>(response: T) {
+  return JSON.parse(JSON.stringify(response));
+}
