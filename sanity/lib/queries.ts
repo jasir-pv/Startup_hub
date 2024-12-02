29b8479d648686c23891930ec0d9b55aee7df631
jasir-pv <jasirahsanpv40@gmail.com>
2024-12-02 23:49:38 +0530

    user page creation

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -58,3 +58,33 @@ export const STARTUPS_QUERY = defineQuery(`*[_type == "startup" && defined(slug.
           bio
       }
       `);
+
+      export const AUTHOR_BY_ID_QUERY = defineQuery(`
+        *[_type == "author" && _id == $id][0]{
+            _id,
+            id,
+            name,
+            username,
+            email,
+            image,
+            bio
+        }
+        `);
+
+
+        export const STARTUPS_BY_AUTHOR = defineQuery(`*[_type == "startup" && author._ref == $id] | order(_createdAt desc) {
+            _id,
+            title,
+            slug,
+            _createdAt,
+            author -> {
+              _id,
+              name,
+              image,
+              bio
+            },
+            views,
+            description,
+            category,
+            image
+          }`);   
